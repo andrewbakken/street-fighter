f6ebf03b04eba928c039c226d06ceb8161770a52
andrewbakken <andrewbakken@mac.com>
2015-04-06 19:17:36 -0400

    initial commit + working

---
+++ js/app.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+    
+    $(document).keydown(function (x) {
+      if ( x.keyCode == 88 ) {
+        $('.ryu-cool').show();
+        $('.ryu-ready').hide();
+        $('.ryu-still').hide();
+      }
+    })
+
+    $(document).keyup(function (x) {
+      if ( x.keyCode == 88 ) {
+        $('.ryu-cool').hide();
+        $('.ryu-still').show();
+      }
+    })
+
+
+    $('.ryu').mouseenter(function() {
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+
+  .mouseleave(function() {
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+  })
+
+ .mousedown(function() {
+    playHadouken();
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').finish().show()
+  	.animate(
+	    {'left': '1020px'},
+	    500,
+	    function() {
+	      $(this).hide();
+	      $(this).css('left', '520px');
+	    }
+	  );
+
+  })
+  .mouseup(function() {
+  $('.ryu-throwing').hide();
+  $('.ryu-ready').show();
+});
+});
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.1;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
+
